e93945d210f1e003b7e3f8be724af7312d455244
GANDUWARD <111099929+GANDUWARD@users.noreply.github.com>
2023-02-08 18:26:01 +0800

    Add files via upload

---
+++ sessions-redis/sessions-redis.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/redis"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	//初始化基于Redis的存储引擎
+	store, _ := redis.NewStore(10, "tcp", "localhost:6379", "", []byte("password"))
+	r.Use(sessions.Sessions("mysession", store))
+
+	r.GET("/incr", func(c *gin.Context) {
+		session := sessions.Default(c)
+		var count int
+		v := session.Get("count")
+		if v == nil {
+			count = 0
+		} else {
+			count = v.(int)
+			count++
+		}
+		session.Set("count", count)
+		session.Save()
+		c.JSON(200, gin.H{"count": count})
+	})
+	r.Run(":8088")
+}
